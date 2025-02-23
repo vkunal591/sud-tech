38030e5a25ac41e2ea8289150ed5cd5f72200470
Kunal Verma <vkunal591@gmail.com>
2025-02-23 22:25:10 +0530

    scrollbar of table

--- src/components/common/table/TableComponent.tsx
+++ src/components/common/table/TableComponent.tsx
@@ -143,7 +143,7 @@ const Table: React.FC<TableProps> = ({
   };
 
   return (
-    <div className="overflow-x-scroll no-scrollbar">
+    <div className="overflow-x-scroll no-scrollba">
       <table className="min-w-full bg-whiteBg">
         <thead>
           <tr className="whitespace-nowrap">
